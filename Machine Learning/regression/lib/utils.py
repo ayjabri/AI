2588f0fea29cf238e2291219c93fe2c9e0722f47
Ayman Jabri <ayman.aljabri@gmail.com>
2021-11-21 18:21:25 -0500

    minor changes

--- Machine Learning/regression/lib/utils.py
+++ Machine Learning/regression/lib/utils.py
@@ -1,12 +1,17 @@
-from os import path
-from urllib import request
+import os
+import urllib
 import tarfile
+import pandas as pd
 
 
-def fetch_online_zip_file(URL, filename):
+def fetch_online_zip_file(URL, filename, as_frame=True, overwrite=False):
     """Download a zipped file from the internetand extract it in the active folder"""
-    request.urlretrieve(path.join(URL,filename), filename)
-    file_tgz = tarfile.open(filename)
-    file_tgz.extractall('.')
-    file_tgz.close()
+    if os.path.exists(filename) and not overwrite:
+        urllib.request.urlretrieve(os.path.join(URL,filename), filename)
+        file_tgz = tarfile.open(filename)
+        file_tgz.extractall('.')
+        file_tgz.close()
+    if as_frame:
+        csv_filename = os.path.splitext(filename)[0] + '.csv'
+        return pd.read_csv(csv_filename)
     pass

02f0cad8f31761d93aa84506b83332f435e77394
Ayman Jabri <ayman.aljabri@gmail.com>
2021-11-24 08:41:19 -0500

    minor changes

--- Machine Learning/regression/lib/utils.py
+++ Machine Learning/regression/lib/utils.py
@@ -1,11 +1,12 @@
+#
 import os
 import urllib
 import tarfile
 import pandas as pd
-
+from hashlib import sha1
 
 def fetch_online_zip_file(URL, filename, as_frame=True, overwrite=False):
-    """Download a zipped file from the internet and extract it in current active folder"""
+    """Download a zipped file from the internet and extract it in current folder"""
     if (not os.path.exists(filename) or overwrite):
         urllib.request.urlretrieve(os.path.join(URL,filename), filename)
         file_tgz = tarfile.open(filename)
@@ -15,3 +16,7 @@ def fetch_online_zip_file(URL, filename, as_frame=True, overwrite=False):
         csv_filename = os.path.splitext(filename)[0] + '.csv'
         return pd.read_csv(csv_filename)
     pass
+
+
+
+def hash_and_split(df, test_size):

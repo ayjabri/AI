01490bbfe3ae088667e90893226f23614a56c384
Ayman Jabri <ayman.aljabri@gmail.com>
2021-12-09 16:06:14 -0500

    create fill-NA module

---
+++ Machine Learning/Regression/lib/_fill_NA_.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 09:26:16 2021
+
+@author: ayman
+"""
+
+from sklearn.impute import SimpleImputer
+
+
+imp = SimpleImputer(strategy='median')
